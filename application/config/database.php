4437ec40846e6284a07a130c66e11facb3417579
Dheeraj Kumar p <dheerajdp777@gmail.com>
2025-05-21 15:50:17 +0530

    good here

--- application/config/database.php
+++ application/config/database.php
@@ -62,55 +62,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$hostname = $_SERVER['HTTP_HOST']; // Get the current host
-
-if ($hostname == 'localhost' || $hostname == '127.0.0.1') {
-    // Local environment
-    $db['default'] = array(
-        'dsn'   => '',
-        'hostname' => 'localhost',
-        'username' => 'root',
-        'password' => '',
-        'database' => '1pc',
-        'dbdriver' => 'mysqli',
-        'dbprefix' => '',
-        'pconnect' => FALSE,
-        'db_debug' => (ENVIRONMENT !== 'production'),
-        'cache_on' => FALSE,
-        'cachedir' => '',
-        'char_set' => 'utf8',
-        'dbcollat' => 'utf8_general_ci',
-        'swap_pre' => '',
-        'encrypt' => FALSE,
-        'compress' => FALSE,
-        'stricton' => FALSE,
-        'failover' => array(),
-        'save_queries' => TRUE
-    );
-} else {
-    // Production environment
-    $db['default'] = array(
-        'dsn'   => '',
-        'hostname' => 'localhost',
-        'username' => 'u824676608_1percentcharit',
-        'password' => 'Charity@123',
-        'database' => 'u824676608_1percentcharit',
-        'dbdriver' => 'mysqli',
-        'dbprefix' => '',
-        'pconnect' => FALSE,
-        'db_debug' => (ENVIRONMENT !== 'production'),
-        'cache_on' => FALSE,
-        'cachedir' => '',
-        'char_set' => 'utf8',
-        'dbcollat' => 'utf8_general_ci',
-        'swap_pre' => '',
-        'encrypt' => FALSE,
-        'compress' => FALSE,
-        'stricton' => FALSE,
-        'failover' => array(),
-        'save_queries' => TRUE
-    );
-}
 $db['default'] = array(
 	'dsn'	=> '',
 
